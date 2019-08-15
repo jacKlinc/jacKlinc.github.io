dc9c4de75a87475c1478dfbf5a034e42a5d44ef8
jacKlinc <jackharding321@gmail.com>
2019-08-15 15:15:20 +0100

    MDN CSS almost finished, JavaScript started on Udacity

---
+++ 2: Basics/C-JS/ref.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+
+// Escaping Strings
+console.log("This allows for quotes \"inside strings.\"");
+
+// Comparing Strings
+if ("jack" == "Jack") { // J != j
+    console.log(false);
+}
+
